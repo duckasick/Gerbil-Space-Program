3edc5a69d9799032b32728be85c04187b874f78e
JornUf <jornuf2901@hotmail.nl>
2025-01-25 16:27:47 +0100

    aaaaaaDWAQFWEFWEF

--- Assets/Testrotate.cs
+++ Assets/Testrotate.cs
@@ -13,6 +13,7 @@ public class Testrotate : MonoBehaviour
     public float verticalSpeed = 2.0f;
 
     public Transform cam;
+    public float angle = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,12 +24,16 @@ public class Testrotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.up = -planet.GetGravityDirection(body);
 
-        //float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        //float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        angle += 5 * h * Time.deltaTime;
         //transform.Rotate(0, h, 0);
-        
-        transform.rotation = Quaternion.LookRotation(new Vector3(cam.forward.x, 0, cam.forward.z), -planet.GetGravityDirection(body));
+        transform.localRotation = Quaternion.Euler(transform.localRotation.x,angle, transform.localRotation.z);
+
+        transform.position = body.transform.position;
+        transform.up = -planet.GetGravityDirection(body);
+
+
+        //transform.rotation = Quaternion.LookRotation(new Vector3(cam.forward.x, 0, cam.forward.z), -planet.GetGravityDirection(body));
     }
 }
